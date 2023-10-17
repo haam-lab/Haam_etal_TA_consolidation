c3c20e8b42ddf37dd75051dd205316afa7f52ad5
Juhee Haam <jhaam1@lsu.edu>
2023-10-16 22:01:49 -0500

    ROI added, dil/ero applied on frames, disk strel used

--- ML/ml1_get_activity_index_batch.m
+++ ML/ml1_get_activity_index_batch.m
@@ -33,8 +33,8 @@ user= getenv('username');
 startFolder = ['C:\Users\' user];
 
 projectFolder = uigetdir(startFolder,'Select a project folder (Parent directory Where Input and Ouput folders Are Stored)');
-motion_index_folder = [projectFolder '\activity_index\'];
-[~,~,~] = mkdir(motion_index_folder);
+activity_index_folder = [projectFolder '\activity_index\'];
+[~,~,~] = mkdir(activity_index_folder);
 
 %% Select a video file when a pop-up window shows up
 % [fileName, pathName] = uigetfile('*.avi', 'Select a video file', groupFolder);
@@ -52,31 +52,38 @@ fileCount = size(inFileName,2);
 for mm = 1:fileCount
 name = regexp(inFileName{mm},'(.*).mp4','tokens');    % remove .mp4
 fileInfo = name{1,1}{1,1};
-
 v = VideoReader([pathName inFileName{mm}]); 
-
 len = v.NumFrames;
 vidH = v.Height;
 vidW = v.Width;
-img_size = vidH * vidW*(down_factor)^2;
 %% Calculate activity index by reading each frame (part or the whole video)
-endFrame = len; % no trimming
-% endFrame = 2000; % optional trimming if a speicfic endframe is put in here
+% Set an ROI for video analysis
+roiPos = round([186.0847 246.0370 855.3699 468.1623]); % default
+% (OPTIONAL) Draw a rectangle to designate a new ROI for analysis
+% frame = 10; % Default frame number for ROI
+% figure; video1= read(v, frame);
+% imshow(video1);
+% roi = drawrectangle;
+% roiPos = round(roi.Position);
+
+endFrame = len; 
+tic;
 activity_index = zeros(endFrame-1,1);
-se = strel('square', 3); % this defines the neighbors
+se = strel('disk', 5); % this defines the neighbors
+
 v.CurrentTime = 0; % initiate the video
 clear frame_prev
 k = 1;
 activity_prev = false(960,1280); % initialize for the second frame.
 while hasFrame(v)
-    frame_cur = readFrame(v);
-    frame_cur=  0.2989 * frame_cur(:,:, 1) + 0.5870 * frame_cur(:,:, 2) + 0.1140 * frame_cur(:,:, 3); 
+    frame_cur = readFrame(v); frame_cur = imcrop(frame_cur, roiPos);
+    frame_cur = rgb2gray(frame_cur); 
     frame_cur = imgaussfilt(frame_cur, sigma);
+    frame_cur = imdilate(frame_cur, se);
+    frame_cur = imerode(frame_cur, se);
     if k > 1
         diff = frame_cur-frame_prev;
         activity = abs(diff) > activity_thr;
-        activity = imerode(activity,se);   
-        activity = imdilate(activity, se); 
         activity_count =  find(activity==1); 
         if isempty(activity_count)==1
             activity_index(k-1) = 0;
@@ -89,22 +96,23 @@ while hasFrame(v)
 end
     
 % normalize the activity_index to the image size and scale
-activity_index = activity_index/img_size*100*2; 
+img_size = roiPos(3)*roiPos(4);
+activity_index = activity_index/img_size*100; 
 
-% plot
+toc;
 len = length(activity_index);
 time = 1/fs/2 : 1/fs :1/fs*(len-1) + 1/fs/2;
 H0 = figure; set(gcf,'position', [104 45 1400 600]);
-plot(time, activity_index,'k'); set(gca,'fontSize',12); xlim('tight');
+plot(time, activity_index,'k'); set(gca,'fontSize',12); xlim('tight'); 
 set(gca,'color','none','XColor','k','YColor','k','FontSize',16, 'GridLineStyle','none');box off; 
-title(['Activity index-' fileInfo '-dil-sigma, ' num2str(sigma)]); 
+title(['Activity index-' fileInfo '-dil-sigma, ' num2str(sigma) '-thr' num2str(activity_thr) '-v1_3_8']); 
 ylabel('Activity Index'); 
 yline(std(activity_index)*2,'r--');
 
 %% Save the output file
-activity_outName = [motion_index_folder fileInfo '_activity_index.csv'];
+activity_outName = [activity_index_folder fileInfo '_activity_index.csv'];
 writematrix([time' activity_index], activity_outName);
-saveas(H0,[motion_index_folder fileInfo '_activity_Fig'],'fig');
+saveas(H0,[activity_index_folder fileInfo '_activity_Fig'],'fig');
 fprintf('Analysis for %s is completed \n', inFileName{mm});
 
 end
